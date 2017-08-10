ccefb34cd666ee3b09bfc3e2da6c7f1b8776e382
Goutham7 <30681782+Goutham7@users.noreply.github.com>
2017-08-10 21:09:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,19 @@ request.onreadystatechange = function () {
  request.open('GET', 'http://taazgoutham7.imad.hasura-app.io/counter', true);
  request.send(null);
 };
+
+//sumbit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  //Make a request to the server and send the name
+  //Capture a list of name and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i<names.lenght; i++) {
+        list +- '<li>' + names[i] + '<li/>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
