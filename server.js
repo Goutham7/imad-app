b43ddb3099e9b1b82b18c523f6372dd8eb6d5fe5
Goutham7 <30681782+Goutham7@users.noreply.github.com>
2017-08-10 22:43:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get('/submit-name/:name', function(req,res) {
     //Get the name from the request
     var name = req.params.name;
     
-    name.push(name);
+    names.push(name);
     // JSON: Javascript Object Notation
     res.send(JSON.stringify(names));
     
