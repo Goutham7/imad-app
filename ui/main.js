3c7f547f9466ef5710e1c132a62a734b842d4a60
Goutham7 <30681782+Goutham7@users.noreply.github.com>
2017-08-10 17:12:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,21 @@ var counter = 0;
 button.onclick = function() {
     
 //Make a request to the counter endpoint
-
+var request = new XMLHttpRequest();
 //Capture the repsonse and store it in variable
+request.onreadystatechange = function () {
+    if(request.readyState===XMLHttpRequest.Done){
+    // Take some action
+    if (request.status===200){
+        var counter = request.responseText;        
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+    }
+    // Not done yet
+}
 
-//Render the variable in the correct span
-   counter = counter + 1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
+};
+// make the request
+ request.open('GET', 'http://taazgoutham7.imad.hasura-app.io/counter', true);
+ request.send(null);
 };
